24a7ac2bbe8b67add5adc3e9385ead63d2134591
askprateek <askpk123@gmail.com>
2015-07-10 01:21:36 +0530

    Localhost Fix

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,7 +4,7 @@ function login(){
     password : $("#login .password").val()
   };
   var xhr = new XMLHttpRequest({mozSystem:true});
-  xhr.open("POST", "http://127.0.0.1:8080/annonetwork/signin.php", false);
+  xhr.open("POST", "http://localhost/annonetwork/backend/signin.php", false);
   xhr.send(JSON.stringify(login_packet));
   if(xhr.responseText == "true"){
    console.log("Login successful, redirecting to home.html");
@@ -19,18 +19,18 @@ function login(){
 }
 
 function signup(){
-  
+
    var signup_packet = {
     username : $("#signup .username").val(),
     password : $("#signup .password").val()
   };
-  
+
   var xhr = new XMLHttpRequest({mozSystem:true});
-  xhr.open("POST", "http://127.0.0.1:8080/annonetwork/register.php", false);
+  xhr.open("POST", "http://localhost/annonetwork/backend/register.php", false);
   xhr.send(JSON.stringify(signup_packet));
-  
+
  if(xhr.responseText == "true"){
-   console.log("Sign up successful, redirecting to home.html"); 
+   console.log("Sign up successful, redirecting to home.html");
    localStorage.setItem("username", $("#signup .username").val());
    console.log('hey'+localStorage.getItem('username'));
    window.location = "home.html";
@@ -49,7 +49,7 @@ function addTag(){
     $("#tagRow").append(value+' ');
     $('#channels').val("");
   }
-   
+
 }
 
 function postQue(){
@@ -57,14 +57,14 @@ function postQue(){
     username : "check",//localStorage.getItem("username"),
     quesstat : $('#question').val(),
   }
-  
+
   que_json['tags'] = tags;
   $('#question').val("");
   $('#tagRow').append("<br><br>Question has been posted. Thank you!");
-  
+
   console.log(JSON.stringify(que_json));
-  
+
   var xhr = new XMLHttpRequest({mozSystem:true});
-  xhr.open("POST", "http://127.0.0.1:8080/annonetwork/insert.php", false);
+  xhr.open("POST", "http://localhost/annonetwork/backend/insert.php", false);
   xhr.send(JSON.stringify(que_json));
 }
